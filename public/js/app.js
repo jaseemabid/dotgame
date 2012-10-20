ff0dc85da60e69c911913ab795194b83633dccf9
Jaseem Abid <jaseemabid@gmail.com>
2012-10-21 00:09:13 +0530

    Moving app logic to router

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,13 @@
 (function(app) {
 
-  // Provide a global location to place configuration settings and module
-  // creation.
+	// Init the app
+
+	var root = new app.Router();
+	Backbone.history.start();
+
 	_.extend(app, {
 		// The root path to run the application.
-		root: "/"
-	});
-
-	var board = new app.collections.Board({
-			size : 3
-		}),
-		dot = new app.models.Box(),
-		point = new app.views.Box({
-			model : dot
-		});
-
-
-
-
-
-	_.extend(dotgame, {
-		board : board
+		root : root
 	});
 
 }(dotgame));
--- public/js/router.js
+++ public/js/router.js
@@ -1,16 +1,29 @@
 (function(app) {
 
-  // Defining the application router, you can attach sub routers here.
-  app.Router = Backbone.Router.extend({
-    routes: {
-		"" : "index"
-    },
+	// Defining the application router, you can attach sub routers here.
+	app.Router = Backbone.Router.extend({
+		routes: {
+			"" : "index"
+		},
+		index: function() {
 
-    index: function() {
-		console.log("Home page");
-    }
-  });
+			console.log("Index");
 
-  return app.Router;
+			var board = new app.collections.Board({
+				size : 3
+			}),
+				dot = new app.models.Box(),
+				point = new app.views.Box({
+					model : dot
+				});
+
+			_.extend(dotgame, {
+				board : board
+			});
+
+		}
+	});
+
+	return app.Router;
 
 }(dotgame));
